a6853340d1a19024c42179d4a875e5f0efa0e9bd
Susan <sttabassum93@gmail.com>
2019-12-18 16:04:11 -0500

    Completed Person class

--- src/main/java/com/github/perschola/Pet.java
+++ src/main/java/com/github/perschola/Pet.java
@@ -4,7 +4,12 @@ package com.github.perschola;
  * Created by leon on 12/17/2019.
  */
 public class Pet {
+    private String name;
+    private Integer age;
+
     public Pet() {
+        this.name = "";
+        this.age = 0;
     }
 
     public Pet(String name, Integer age) {

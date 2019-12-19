d133b3940bcdda9d54f063ff9e5984090535bcc2
Susan <sttabassum93@gmail.com>
2019-12-19 15:02:32 -0500

    Complete

--- src/main/java/com/github/perschola/Leon.java
+++ src/main/java/com/github/perschola/Leon.java
@@ -3,5 +3,13 @@ package com.github.perschola;
 /**
  * Created by leon on 12/17/2019.
  */
-public class Leon {
+public class Leon extends PetOwner{
+    private Person person;
+
+    public Leon(){
+        Pet milo = new Pet("Milo", 6);
+        super.setPet(milo);
+        super.setFirstName("Leon");
+        super.setLastName("Hunter");
+    }
 }
